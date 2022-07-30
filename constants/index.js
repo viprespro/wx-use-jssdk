6b196d6de492c82677ff8f75379c3361737fad66
Ares <244570102@qq.com>
2022-07-30 11:48:31 +0800

    docs：update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: 'wxa6c30b0075d4adf9',
   secret: '4257c6587844598f8270117cd122d29d',
-  timestamp: 1414587457, // 与微信文档一样
-  nonceStr: 'Wm3WZYTPz0wzccnW', // 与微信文档一样
-  baseUrl: '/cgi-bin', // 本地devServer代理 线上nginx作的代理
+  timestamp: 1414587457, // 与微信官方文档保持一致
+  nonceStr: 'Wm3WZYTPz0wzccnW', // 与微信官方文档保持一致
+  baseUrl: '/cgi-bin', // 本地devServer 线上nginx代理
 }
